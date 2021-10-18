eab722581a56ba0a2decca783f5aae42a119a6f9
sean <seanpatel076@gmail.com>
2021-10-18 18:21:26 -0400

    hashing

--- 02-Hashing_with_Hashlib/Unsolved/app.py
+++ 02-Hashing_with_Hashlib/Unsolved/app.py
@@ -27,25 +27,25 @@ import streamlit as st
 
 # @TODO:
 # Define a function called `hash_data` that take in a parameter called `data`
-# YOUR CODE HERE!
+def hash_data(data):
 
 # @TODO:
 # Instantiate an instance of hashlib's `sha256` function
-sha =  # YOUR CODE HERE!
+    sha =  hashlib.sha256()
 
 # @TODO:
 # Use the `encode` function to encode the string version of the data that
 # was passed in as a parameter to the function
-encoded_data =  # YOUR CODE HERE!
+    encoded_data =  str(data).encode()
 
 # @TODO:
 # Call the hashing instance and the `update` function. Pass it the encoded
 # data as a parameter
-# YOUR CODE HERE!
+    sha.update(encoded_data)
 
 # @TODO:
 # Return the unique hash of the data using the `hexidigest` function
-# YOUR CODE HERE!
+    return sha.hexdigest()
 
 ################################################################################
 # Streamlit Code
@@ -61,12 +61,11 @@ st.markdown("# Create a Unique Hash of Data")
 # Add a Streamlit `text_area` component to accept data from the user
 # Be sure to convert the input data to a string
 # Use the `encode` function to encode the input data
-input_data =  # YOUR CODE HERE!
-
+input_data =  str(st.text_area("Enter Data")).encode()
 # @TODO:
 # Use the Streamlit `write` function to display the length (`len) of the input
 # data back to the user
-# YOUR CODE HERE!
+st.write(f"Input Length: {len(input_data)}")
 
 ################################################################################
 # Step 3:
@@ -76,15 +75,16 @@ input_data =  # YOUR CODE HERE!
 
 # @TODO:
 # Add a Streamlit `button` named “Hash Text”
-# YOUR CODE HERE!
+if st.button("Hash Text"):
 
 # @TODO:
 # Generate a hash of the user input using the `hash_data` function
-input_hash =  # YOUR CODE HERE!
+    input_hash =  hash_data(input_data)
 
 # @TODO:
 # Use the Streamlit `write` function to display the unique hash of the data
-# YOUR CODE HERE!
+    st.write(f"Output Hash (fingerprint): {input_hash}" )
+    st.write(f"Output Length: {len(input_hash)}")
 
 # @TODO:
 # Use the Streamlit `write` function to display the length of the output
